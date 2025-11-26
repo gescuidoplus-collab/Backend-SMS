95b34b99b6af720a92005ef7b7fd6b2ab161943d
gescuidoplus-collab <gescuidoplus@gmail.com>
2025-11-26 15:12:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const app = express();
 const ai = new GoogleGenAI({})
-
+//
 
 app.use(express.json());
 
